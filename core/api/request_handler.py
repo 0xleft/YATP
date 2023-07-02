2165935f8a05819f04db6781d41d4469c78391e4
AdomasDauda <107749872+AdomasDauda@users.noreply.github.com>
2023-07-02 17:47:55 +0100

    update prompt
    so its more consistent

--- core/api/request_handler.py
+++ core/api/request_handler.py
@@ -3,7 +3,13 @@ import json
 from requests import post, Response
 
 prompt = """
-You need to make an executive summary in english referencing all the photos. The executive summary is about a building site and what is wrong with the building site. The what is wrong part is more important. After the summary you need to make a list of things that were bad, referencing the photos. You should only mention one issue that is summary of a few photos and if the issue is not associated with other issues you should mention it differently. Just keep in mind that Idem means 'the same as the last one'.  Do not use italian words. An executive summary should be only  a short paragraph. Try to be as concise as possible. DO NOT INCLUDE ANY DISCLAIMERS. Do not say P1, P2 say Photo 1, Photo 2. Try to talk about issues very shortly. Ok means good. Do not forget to list the issues at the bottom. Smoking is not permitted on a building site and should be treated as a separate issue. DO NOT mention photos in the executive summary you should only summarize the descriptions. Only the part where you list should mention photos. If there are good things you should also mention them in the summary. MENTION PHOTOS ONLY IN THE LIST AND NOT IN THE PARAGRAPH. If one photo is idem group it up the the last one. DO NOT MENTION PHOTOS THAT DO NOT EXIST.
+You need to make an executive summary in english referencing all the photos. The executive summary is about a building site and what is wrong with the building site. The what is wrong part is more important. After the summary you need to make a list of things that were bad, referencing the photos. You should only mention one issue that is summary of a few photos and if the issue is not associated with other issues you should mention it differently. Just keep in mind that Idem means 'the same as the last one'.  Do not use italian words. An executive summary should be only a short paragraph. Try to be as concise as possible. DO NOT INCLUDE ANY DISCLAIMERS. Do not say P1, P2 say Photo 1, Photo 2. Try to talk about issues very shortly. Ok means good. Do not forget to list the issues at the bottom. Smoking is not permitted on a building site and should be treated as a separate issue. DO NOT mention photos in the executive summary you should only summarize the descriptions. Only the part where you list should mention photos. If there are good things you should also mention them in the summary. MENTION PHOTOS ONLY IN THE LIST AND NOT IN THE PARAGRAPH. If one photo is idem group it up the the last one. DO NOT MENTION PHOTOS THAT DO NOT EXIST. The list should look something like this:
+1. Bad thing 1 (Photo 1, Photo 2)
+2. Bad thing 2 (Photo 3)
+3. Bad thing 3 (Photo 4)
+4. Bad thing 4 (Photo 5, Photo 6)
+5. Bad thing 5 (Photo 7)
+and so on...
 GENERATE AN EXECUTIVE SUMMARY FOR THIS INPUT: USER_INPUT
 """
 
